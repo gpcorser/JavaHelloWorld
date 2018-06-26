373b9d75563a6f19c83a2f2b68ad3272a16de4b6
gpcorser <gpcorser@SE137LAB-TS.ad.svsu.edu>
2018-06-26 10:09:49 -0400

    added print line

--- JavaHelloWorld/src/javahelloworld/JavaHelloWorld.java
+++ JavaHelloWorld/src/javahelloworld/JavaHelloWorld.java
@@ -17,7 +17,6 @@ public class JavaHelloWorld {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello, World");
-        System.out.println("Ta dah!");
     }
     
 }
